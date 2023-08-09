87f27a4d49826fe75ea394efc30c1c23fd215e9e
Nisarg Dave <ndave630@gmail.com>
2023-08-09 18:47:46 +0800

    Installed graphql yoga server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,25 @@
-console.log("Hello from server!");
+import { createServer } from "node:http";
+import { createYoga, createSchema } from "graphql-yoga";
+// 2
+const port = Number(process.env.API_PORT) || 4000;
+
+const yoga = createYoga({
+  schema: createSchema({
+    typeDefs: /* GraphQL */ `
+      type Query {
+        hello: String
+      }
+    `,
+    resolvers: {
+      Query: {
+        hello: () => "Hello from Yoga!",
+      },
+    },
+  }),
+});
+// 3
+const server = createServer(yoga);
+// 4
+server.listen(4000, () => {
+  console.log(`🚀 GraphQL Server ready at http://localhost:${port}/graphql`);
+});

1097cd3e68bbded50db22b24cf908ea4a245b812
Nisarg Dave <ndave630@gmail.com>
2023-08-12 20:54:37 +0800

    Installed and configured GraphQl codegen

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,5 @@
 import { createServer } from "node:http";
-import { createYoga, createSchema } from "graphql-yoga";
+import { createYoga } from "graphql-yoga";
 import { schema } from "./schema";
 
 const port = Number(process.env.API_PORT) || 4000;

b6772c7129c778fc743c3d43df09781d3977e42c
Nisarg Dave <ndave630@gmail.com>
2023-08-12 21:22:54 +0800

    Modified static types in client to use ones generated from code gen

--- client/src/types.ts
+++ client/src/types.ts
@@ -1,14 +1,9 @@
-export type Tag = {
-  id: string;
-  label: string;
-};
+import type { GetNotesQuery } from "./graphql/generated";
+// The reason you import this type instead of the full Note and Tag types is that the GetNotesQuery type has access to a more specific set of types that contain only the fields your query retrieves
+// Accessing first index as that's where the type definitions are
 
-export type NoteData = {
-  title: string;
-  body: string;
-  tags: Tag[];
-};
+export type Tag = GetNotesQuery["notes"][0]["tags"][0];
 
-export type Note = {
-  id: string;
-} & NoteData;
+export type Note = GetNotesQuery["notes"][0];
+
+export type NoteData = Omit<Note, "id">;
